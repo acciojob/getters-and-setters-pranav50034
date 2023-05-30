e605724ad5cdba72c2815fedb907cfd6f937fdb5
Pranav Pokale <106576132+pranav50034@users.noreply.github.com>
2023-05-30 13:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,16 +13,27 @@ class Person {
 }
 
 class Student extends Person {
+	constructor(name, age){
+		super.name = name;
+		super.age= age;
+	}
 	study(){
 		console.log(`${super.name} is studying`)
+		document.getElementById('p').innertText=`${super.name} is studying`
 	}
 }
 
 class Teacher extends Person {
+	constructor(name, age){
+		super.name = name;
+		super.age= age;
+	}
 	teach(){
 		console.log(`${super.name} is teaching`)
 	}
 }
+
+// let s = new Student('pranav', 56)
 
 // Do not change the code below this line
 window.Person = Person;
