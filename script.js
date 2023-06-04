b1cec8bfc234b3c102d0993dffaea0461147c938
Pranav Pokale <106576132+pranav50034@users.noreply.github.com>
2023-06-04 21:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-//complete this code
 class Person {
-	constructor(name, age){
-		this.name = name;
-		this.age= age;
-	}
-	getName(){
-		return this.name
-	}
-	setAge(x){
-		this.age = x;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-	constructor(name, age){
-		super.name = name;
-		super.age= age;
-	}
-	study(){
-		console.log(`${super.name} is studying`)
-		document.getElementById('p').innertText=`${super.name} is studying`
-	}
+  study() {
+    console.log(`${this._name} is studying.`);
+  }
 }
 
 class Teacher extends Person {
-	constructor(name, age){
-		super.name = name;
-		super.age= age;
-	}
-	teach(){
-		console.log(`${super.name} is teaching`)
-	}
+  teach() {
+    console.log(`${this._name} is teaching.`);
+  }
 }
-
-// let s = new Student('pranav', 56)
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
